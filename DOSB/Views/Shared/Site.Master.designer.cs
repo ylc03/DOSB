42ea8ff0d6ff3e06aedc7c5dfe83d9293f492959
ylc03 <yuanlichuan@gmail.com>
2011-01-12 00:08:53 +0300

    Change site.master header and menu
    Signed-off-by: ylc03 <yuanlichuan@gmail.com>

--- DOSB/Views/Shared/Site.Master.designer.cs
+++ DOSB/Views/Shared/Site.Master.designer.cs
@@ -31,15 +31,6 @@ namespace DOSB.Views.Shared {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ScriptContent;
         
         /// <summary>
-        /// Image1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image1;
-        
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
